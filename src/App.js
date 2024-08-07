fef85652fefaef4d9237c4700fa8491f398abfaf
MaciejMl <szypki@gmail.com>
2024-08-07 23:37:56 +0200

    Add Router & create NotFound404 view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Container } from '@mui/material';
-import Home from './components/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import NotFound404 from './components/views/NotFound404/NotFound404';
 
 function App() {
   return (
     <Container>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
     </Container>
   );
 }
---
+++ src/components/views/NotFound404/NotFound404.js
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+
+const NotFound404 = () => {
+  return (
+    <Container>
+      <h1>Page NOT FOUND - error 404</h1>
+    </Container>
+  );
+};
+
+export default NotFound404;
