63b68074ff63149a6a43037e401e8e8ebf965559
MaciejMl <szypki@gmail.com>
2024-08-09 01:15:32 +0200

    Add firebaseConfig secrets in firebase.js to .env file

--- src/components/features/Scheduler/Scheduler.js
+++ src/components/features/Scheduler/Scheduler.js
@@ -1,26 +1,29 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
+import { collection, getDocs } from 'firebase/firestore';
 import {
   Scheduler,
   DayView,
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
+import { db } from '../../../firebase';
+import { useState, useEffect } from 'react';
 
 const Callendar = () => {
-  const currentDate = '2018-11-01';
-  const schedulerData = [
-    {
-      startDate: '2018-11-01T09:45',
-      endDate: '2018-11-01T11:00',
-      title: 'Meeting',
-    },
-    {
-      startDate: '2018-11-01T12:00',
-      endDate: '2018-11-01T13:30',
-      title: 'Go to a gym',
-    },
-  ];
+  const [schedulerData, setSchedulerData] = useState([]);
+  const currentDate = '2024-08-10';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataFromDB = await getDocs(collection(db, 'appointments'));
+      const appointments = dataFromDB.docs.map((doc) => doc.data());
+      setSchedulerData(appointments);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Paper>
       <Scheduler data={schedulerData}>
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 //import { getAnalytics } from 'firebase/analytics';
-const { initializeApp } = require('firebase/app');
-const { getFirestore } = require('firebase/firestore');
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAL3KDabF-79AOjQaigqnh1rBi76Y14bCY',
-  authDomain: 'myjobapp-e1c81.firebaseapp.com',
-  projectId: 'myjobapp-e1c81',
-  storageBucket: 'myjobapp-e1c81.appspot.com',
-  messagingSenderId: '923544251824',
-  appId: '1:923544251824:web:7efd3e216cbb13d5ac3a75',
-  measurementId: 'G-PBRJHH2KMH',
+  apiKey: process.env.REACT_APP_SECRET_apiKey,
+  authDomain: process.env.REACT_APP_SECRET_authDomain,
+  projectId: process.env.REACT_APP_SECRET_projectId,
+  storageBucket: process.env.REACT_APP_SECRET_storageBucket,
+  messagingSenderId: process.env.REACT_APP_SECRET_messagingSenderId,
+  appId: process.env.REACT_APP_SECRET_appId,
+  measurementId: process.env.REACT_APP_SECRET_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 const db = getFirestore(app);
-module.exports = { db };
+export { db };
